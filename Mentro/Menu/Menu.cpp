c9f08ba342557c2d8d8849491885b668d9c3b1ce
hgoel0974 <hgoel0974@gmail.com>
2014-06-23 01:16:43 -0400

    Implemented Waves and wallpaper

---
+++ Mentro/Menu/Menu.cpp
@@ -0,0 +1,128 @@
+#include "Menu.h"
+
+#pragma region
+char *appPath, *toLaunchPath;
+int toLaunchType, toLaunchExtra;
+
+Section *sections[MAX_SECTIONS];
+u8 sectId = 0;
+
+u8 curSectId = 0;
+u8 newSectId = 0;
+
+u8 ShouldExit = 0;
+u8 updatePending = 0;
+#pragma endregion
+
+void Menu::setAppPath(char *path)
+{
+	appPath = path;
+	*(strrchr(appPath, '/') + 1) = 0x0;	//Terminate app path at last directory before eboot
+}
+
+void Menu::RegisterSection(Section *section)
+{
+	//Only register a section if a free slot is available
+	if (sectId < MAX_SECTIONS){
+		sections[sectId] = section;
+		sectId++;
+	}
+}
+
+void Menu::TransitionSection(u8 sectToTransition)
+{
+	newSectId = sectToTransition;
+	updatePending = 1;	//The menu system needs to update its transition routines
+}
+
+//Slide Transition effect 
+u16 frames = 0;
+u8 TransitionEffect()
+{
+	if (frames < 480)
+	{
+		//Slide transition while not crossed the entire screen
+		frames++;
+		sections[curSectId]->Render(frames, 0);
+		sections[newSectId]->Render(480 - frames, 0);
+
+		return 1;
+	}
+	else{
+		frames = 0;
+		//Apply the update and resume normal functions
+		curSectId = newSectId;
+		newSectId = 0;
+		return 0;
+	}
+}
+
+#pragma region
+void Menu::Launch(char* appPath, int appType, int appExtra)
+{
+	toLaunchPath = appPath;
+	toLaunchType = appType;
+	toLaunchExtra = appExtra;
+	ShouldExit = 1;
+}
+
+int Menu::getAppType()
+{
+	return toLaunchType;
+}
+
+int Menu::getExtraInfo()
+{
+	return toLaunchExtra;
+}
+#pragma endregion
+
+char* Menu::Start()
+{
+	//Load Resources
+	oslInit(0);
+	oslIntraFontInit(INTRAFONT_CACHE_ALL | INTRAFONT_STRING_UTF8);
+	oslInitGfx(OSL_PF_8888, 1);
+	oslInitAudio();
+	oslSetQuitOnLoadFailure(1);
+	oslSetKeyAutorepeatInit(40);
+	oslSetKeyAutorepeatInterval(10);
+
+	u32 shudSkip = 0;
+
+	Wave::Setup(RGBA(228, 69, 69, 200), RGBA(255, 255, 255, 0));
+
+	OSL_IMAGE *wall = oslLoadImageFilePNG("WALL.PNG", OSL_IN_VRAM, OSL_PF_5650);
+
+	//Menu Loop
+	while (!ShouldExit)
+	{
+		if (shudSkip)goto needToSkip;
+
+		oslStartDrawing();
+		
+		oslDrawImageXY(wall, 0, 0);
+
+		//If an update is pending, transition
+		if (updatePending)
+		{
+			updatePending = TransitionEffect();
+		}
+		else{
+			Wave::Update();
+			//sections[curSectId]->Update(0, 0);
+
+			Wave::Render();
+			//sections[curSectId]->Render(0, 0);
+		}
+
+		oslEndDrawing();
+
+	needToSkip:
+		shudSkip = oslSyncFrame();
+	}
+
+	//Free Resources
+
+	return toLaunchPath;
+}
--- Mentro/Menu/Wave.cpp
+++ Mentro/Menu/Wave.cpp
@@ -1,17 +1,31 @@
 #include "Wave.h"
 
+OSL_COLOR waveTop, waveBottom;
+u16 waveProgression;
 
 void Wave::Render()
 {
+	for (int x = 0; x <= 480; x+=2)
+	{
+		int y = 30 * oslSin(waveProgression/4 + x, 1) + 100;
+		int y2 = 50 * oslSin(waveProgression / 2 + x, 1) + 100;
 
+		oslDrawGradientRect(x, 0, x + 2, y2, waveBottom, waveBottom, waveTop, waveTop);
+		oslDrawGradientRect(x, y2, x + 2, 272, waveTop, waveTop, waveBottom, waveBottom);
+
+		oslDrawGradientRect(x, 0 , x + 2, y, waveBottom, waveBottom, waveTop, waveTop);
+		oslDrawGradientRect(x, y, x + 2, 272, waveTop, waveTop, waveBottom, waveBottom);
+	}
 }
 
 void Wave::Update()
 {
-
+	waveProgression++;
+	if (waveProgression >= 360 * 4)waveProgression = 0;
 }
 
 void Wave::Setup(u32 col1, u32 col2)
 {
-
+	waveTop = col1;
+	waveBottom = col2;
 }
